28ad4dab010e1a59a92fa303f7ca452c746fa32e
pisithsork <55104592+pisithsork@users.noreply.github.com>
2023-04-17 11:56:39 -0400

    Minor Changes

--- Week_4-17/CodingChallenge/CodingChallenge/Solutions.cs
+++ Week_4-17/CodingChallenge/CodingChallenge/Solutions.cs
@@ -7,11 +7,12 @@ using System.Threading.Tasks;
 namespace CodingChallenge
 {
     public class Solutions
-    {            /* Thought Process:
+    {           
+        public int[] IndexofCapitals(string input)
+        { 
+            /* Thought Process:
              *  What I can do is have a foreach/for loop, convert each letter to char and check if the value lies between 65 and 90 as these values will be only uppercase letters. If the edge cases includes special character than that is still fine. If this condition is true then take the index and add it to the result[]. After the foreach/for loop is done return the result.
              */
-        public int[] IndexofCapitals(string input)
-        {
 
             List<int> result = new List<int>();
             foreach(char c in input)
