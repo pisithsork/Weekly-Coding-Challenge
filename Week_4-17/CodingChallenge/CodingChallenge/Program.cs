c2a328d1331769e4cfce481e587ef4f2be8cb0d0
pisithsork <55104592+pisithsork@users.noreply.github.com>
2023-04-17 11:54:12 -0400

    Completed Monday 4-17 Challenge

---
+++ Week_4-17/CodingChallenge/CodingChallenge/Program.cs
@@ -0,0 +1,25 @@
+﻿using CodingChallenge;
+using System;
+Solutions solution = new Solutions();
+Console.WriteLine("Revature Coding Challenge: ");
+Console.WriteLine("04/17/2023 Challenge: Create a function that takes a single string as argument and returns an ordered array containing the indices of all capital letters in the string.");
+/*
+ Examples
+
+IndexOfCapitals("eDaBiT") ➞ [1, 3, 5]
+
+IndexOfCapitals("eQuINoX") ➞ [1, 3, 4, 6]
+
+IndexOfCapitals("determine") ➞ []
+
+IndexOfCapitals("STRIKE") ➞ [0, 1, 2, 3, 4, 5]
+
+IndexOfCapitals("sUn") ➞ [1]
+ */
+string[] inputs = { "eDaBiT", "eQuINoX", "determine", "STRIKE", "sUn" };
+foreach(string input in inputs)
+{
+    var result = solution.IndexofCapitals(input);
+    Console.WriteLine("IndexOfCapitals(\"{0}\") => [{1}]", input, string.Join(", ", result));
+}
+
---
+++ Week_4-17/CodingChallenge/CodingChallenge/Solutions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingChallenge
+{
+    public class Solutions
+    {            /* Thought Process:
+             *  What I can do is have a foreach/for loop, convert each letter to char and check if the value lies between 65 and 90 as these values will be only uppercase letters. If the edge cases includes special character than that is still fine. If this condition is true then take the index and add it to the result[]. After the foreach/for loop is done return the result.
+             */
+        public int[] IndexofCapitals(string input)
+        {
+
+            List<int> result = new List<int>();
+            foreach(char c in input)
+            {
+                if (Char.IsUpper(c))
+                {
+                    result.Add(input.IndexOf(c));
+                }
+            }
+            return result.ToArray();
+
+        }
+    }
+}
