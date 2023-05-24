8896bf10c17ba7d728024a0570d68d51f6b8d55f
pisithsork <55104592+pisithsork@users.noreply.github.com>
2023-05-24 12:08:31 -0400

    Completed 5/24 Weekly Coding Challenge

---
+++ Week_5-23/CodingChallenge/CodingChallenge_5-23/CodingChallenge_5-23/Program.cs
@@ -0,0 +1,28 @@
+﻿using CodingChallenge_5_23;
+using System;
+
+Solution solution = new Solution();
+Console.WriteLine("Revature Coding Challenge: ");
+Console.WriteLine("05/24/23 Challenge: Write a program that accepts sets of three numbers, and prints the second-maximum number among the three.");
+
+int[][] inputs =
+{
+    new int[] { 1, 2, 3 },
+    new int[] { 10, 15, 5, },
+    new int [] {100, 999, 500 }
+};
+foreach (int[] input in inputs)
+{
+    var result = solution.SecondMaxValue(input);
+    Console.WriteLine("SecondMaxValue({{{0}, {1}, {2}}}) => [{3}]", input[0], input[1], input[2], string.Join(", ", result));
+}
+
+
+
+Console.WriteLine("Revature Coding Challenge: ");
+Console.WriteLine("05/24/23 Challenge: Given an alphanumeric string made up of digits and lower case Latin characters only, find the sum of all the digit characters in the string.");
+
+string input2 = "ab1234cd";
+
+var result2 = solution.AlphnumericValue(input2);
+Console.WriteLine(result2);
---
+++ Week_5-23/CodingChallenge/CodingChallenge_5-23/CodingChallenge_5-23/Solution.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingChallenge_5_23
+{
+    public class Solution
+    {
+        public int SecondMaxValue(int[] input)
+        {
+            /*
+             * If I am allowed to use functions provided by the library. but if not then I would just retrieve the max value while also tracking the last value to be tracked.
+             * Array.Sort(input);
+             * return input[1];
+             */
+            int holder;
+            for(int i = 0; i < input.Length; i++)
+            {
+                for (int j = i+1; j < input.Length; j++)
+                {
+                    if (input[j] > input[i])
+                    {
+                        holder = input[i];
+                        input[i] = input[j];
+                        input[j] = holder;
+                    }
+                }
+            }
+            return input[1];
+        }
+
+        public int AlphnumericValue(string input)
+        {
+            /*
+             * So what I could do is basically have a foreach loop in where we check to see each char value is in the range of the ASCII code in that it would be considered an actual number and if so convert it to int and add it to the sum value and return the sum
+             */
+            int sum = 0;
+            foreach(char value in input)
+            {
+                //Char.IsDigit, if I can't use the function given to us in the library then instead I would get the ASCII value and see if it is within range of the ASCII value of 0-9
+                if ((int)value >= 48 && (int)value <= 57)
+                {
+                    sum += int.Parse(value.ToString());
+                }
+            }
+
+            return sum;
+        }
+    }
+}
